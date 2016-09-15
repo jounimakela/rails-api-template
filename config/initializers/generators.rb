0a936c3c0e8b680c5937e1d10ae2f58a9fbb3bf4
Jouni Mäkelä <jouni.makela@op.fi>
2016-09-16 02:40:07 +0300

    Customized initializers

---
+++ config/initializers/generators.rb
@@ -0,0 +1,7 @@
+Rails.application.config.generators do |g|
+  g.template_engine :jbuilder
+  g.stylesheets     false
+  g.javascripts     false
+  g.view_specs      false
+  g.helper_specs    false
+end
--- config/template.rb
+++ config/template.rb
@@ -2,6 +2,7 @@ copy_file 'config/environments/development.rb', force: true
 copy_file 'config/environments/production.rb', force: true
 copy_file 'config/application.rb', force: true
 copy_file 'config/database.yml', force: true
+copy_file 'config/initializers/generators.rb', force: true
 
 # Replace $TEMPLATE with the proper project name
 gsub_file 'config/application.rb', '$TEMPLATE', "#{@app_name}".camelize
