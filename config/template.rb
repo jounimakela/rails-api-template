a1eece529527b4ba8e8a23bb24bfaf7ab020f4c2
Jouni Mäkelä <jouni.makela@op.fi>
2016-09-16 02:07:31 +0300

    Initial draft of the application config

--- config/template.rb
+++ config/template.rb
@@ -1,2 +1,5 @@
 copy_file 'config/environments/development.rb', force: true
 copy_file 'config/environments/production.rb', force: true
+copy_file 'config/application.rb', force: true
+
+gsub_file 'config/application.rb', '$TEMPLATE', "#{@app_name}"
