fa32b1a5fe646e8acb47a32a8159d18c49b03db9
Jouni Mäkelä <jouni.makela@op.fi>
2016-09-16 02:04:06 +0300

    Configuration template

--- config/template.rb
+++ config/template.rb
@@ -1,9 +1,2 @@
-copy_file 'config/routes.rb', force: true
-
 copy_file 'config/environments/development.rb', force: true
 copy_file 'config/environments/production.rb', force: true
-
-# Replace test_template with the proper project name
-# gsub_file 'config/environments/production.rb', '$LOG_FILENAME', "#{@app_name}"
-
-copy_file 'config/locales/en.yml', force: true
