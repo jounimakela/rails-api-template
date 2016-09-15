7148d88d2fffbdc930c53abe572e0b8bebb269d5
Jouni Mäkelä <jouni.makela@op.fi>
2016-09-16 02:08:41 +0300

    Database configuration

--- config/template.rb
+++ config/template.rb
@@ -3,3 +3,6 @@ copy_file 'config/environments/production.rb', force: true
 copy_file 'config/application.rb', force: true
 
 gsub_file 'config/application.rb', '$TEMPLATE', "#{@app_name}"
+
+# Replace test_template with the proper project name
+gsub_file 'config/database.yml', '$TEMPLATE', "#{@app_name}".underscore
