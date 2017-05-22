ec0a47328ee0c1dbbbc5f7f4617dec366eec5337
Jouni Mäkelä <jouni.makela@op.fi>
2017-05-22 20:18:01 +0300

    Replace dashes with underscores in application name
    Signed-off-by: Jouni <jouni.img@gmail.com>

--- config/template.rb
+++ config/template.rb
@@ -6,8 +6,8 @@ copy_file 'config/database.yml', force: true
 copy_file 'config/initializers/generators.rb', force: true
 
 # Replace $TEMPLATE with the proper project name
-gsub_file 'config/application.rb', '$TEMPLATE', "#{@app_name}".camelize
-gsub_file 'config/database.yml', '$TEMPLATE', "#{@app_name}".underscore
+gsub_file 'config/application.rb', '$TEMPLATE', "#{@app_name}".gsub("-", "_").camelize
+gsub_file 'config/database.yml', '$TEMPLATE', "#{@app_name}".gsub("-", "_").underscore
 
 # Remove assets.rb if user didnt set --api switch during rails new
 remove_file 'config/initializers/assets.rb'
