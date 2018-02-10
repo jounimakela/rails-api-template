0f58807d9978589ebbbc154078818c7095f5b330
Jouni Mäkelä <jouni.makela@op.fi>
2018-02-11 00:17:14 +0200

    Apply config/environment files
    Signed-off-by: Jouni Mäkelä <jouni.makela@op.fi>

--- config/template.rb
+++ config/template.rb
@@ -1,11 +1,11 @@
-# copy_file 'config/environments/development.rb', force: true
-# copy_file 'config/environments/production.rb', force: true
-# copy_file 'config/environments/test.rb', force: true
-
 copy_file 'config/application.rb', force: true
 copy_file 'config/database.yml', force: true
 copy_file 'config/initializers/generators.rb', force: true
 copy_file 'config/initializers/logger.rb', force: true
+
+copy_file 'config/environments/development.rb', force: true
+copy_file 'config/environments/production.rb', force: true
+copy_file 'config/environments/test.rb', force: true
 
 # Replace $TEMPLATE with the proper project name
 gsub_file 'config/application.rb', '$TEMPLATE', "#{@app_name}".gsub("-", "_").camelize
